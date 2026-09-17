e2b8b4e622601decbb44b6c827d347d2cf80546d
M-racle <108946729+M-racle@users.noreply.github.com>
2026-09-17 20:48:27 +0300

    I added the AuthController for registration and login, and my Register and Login methods for POST operations.

---
+++ TodoApp.API/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Application.DTOs.Auth;
+using TodoApp.Application.Interfaces;
+
+namespace TodoApp.API.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService) {
+            _authService = authService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request) {
+            try {
+                var result =await _authService.RegisterAsync(request);
+
+                return Ok(result);
+            } catch (Exception ex) {
+                return BadRequest(new {
+                    message = ex.Message
+                });
+            }
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request) {
+            try {
+                var result =await _authService.LoginAsync(request);
+
+                return Ok(result);
+            } catch (UnauthorizedAccessException ex) {
+                return Unauthorized(new {
+                    message = ex.Message
+                });
+            } catch (Exception ex) {
+                return BadRequest(new {
+                    message = ex.Message
+                });
+            }
+        }
+    }
+}
