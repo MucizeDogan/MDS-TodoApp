0d928121791f37c7bc6c14f338ef8af253b1569a
M-racle <108946729+M-racle@users.noreply.github.com>
2026-09-17 19:59:30 +0300

    Update API configuration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using TodoApp.Infrastructure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton(TimeProvider.System);
+
+builder.Services
+    .AddIdentityCore<ApplicationUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager();
+
+
 
 var app = builder.Build();
 
